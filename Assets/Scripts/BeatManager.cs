a7e9376f0264679301a691b66cb1e6855c7157af
SquidgyPants <115804504+SquidgyPants@users.noreply.github.com>
2024-11-27 11:21:01 +0100

    Code for beatmanager other method

--- Assets/Scripts/BeatManager.cs
+++ Assets/Scripts/BeatManager.cs
@@ -3,216 +3,42 @@ using System.Threading;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Events;
 
 public class BeatManager : MonoBehaviour
 {
     [SerializeField] private AudioSource song;
+    [SerializeField] private float bpm;
+    [SerializeField] private Intervals[] intervals;
 
-
-    private int crochetsperbar = 4;
-    public float bpm = 115f;
-    public float crochet;
-    public float songPosition;
-    public float deltaSongPos;
-    public float lastHit;
-    public float actualLastHit;
-    private float nextBeatTime = 0.0f;
-    private float nextBarTime = 0.0f;
-    public float offset = 0.5f;
-    public float addOffset;
-    //public static float offsetStatic;
-    public static bool hasOffsetAdjusted = false;
-    public int beatNumber = 0;
-    public int barNumber = 0;
-
-    //[SerializeField] private float bpm;
-    [SerializeField] private AudioSource song;
-    [SerializeField] public TextMeshProUGUI countdown;
-
-    [SerializeField] private Player player;
-
-    [SerializeField] private GameObject ghost;
-    ////keep all the position-in-beats of notes in the song
-    //[SerializeField] float[] notes;
-    ////the index of the next note to be spawned
-    //[SerializeField] int nextIndex = 0;
-    //[SerializeField]
-    ////the current position of the song (in seconds)
-    //float songPosition,
-    ////the current position of the song (in beats)
-    float songPosInBeats,
-    ////the duration of a beat
-    secPerBeat,
-    ////how much time (in seconds) has passed since the song started
-    dsptimesong;
-
-
-
-    void Start()
+    private void Update()
     {
-        dsptimesong = (float)AudioSettings.dspTime;
-        songPosition = (float)(AudioSettings.dspTime - dsptimesong) - offset;
+        foreach (Intervals interval in intervals)
+        {
+            float sampledTime = song.timeSamples / (song.clip.frequency * interval.GetIntervalLength(bpm));
+            interval.CheckForNewInterval(sampledTime);
+        }
+    }
+}
+
+[System.Serializable]
+public class Intervals
+{
+    [SerializeField] private float steps;
+    [SerializeField] private UnityEvent trigger;
+    private int lastInterval = -1;
+
+    public float GetIntervalLength(float bpm)
+    {
+        return 60f / bpm * steps;
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    //[SerializeField] private float bpm;
-    //[SerializeField] private AudioSource song;
-    //[SerializeField] public TextMeshProUGUI countdown;
-
-    //[SerializeField] private Player player;
-
-    //[SerializeField] private GameObject ghost;
-    ////keep all the position-in-beats of notes in the song
-    //[SerializeField] float[] notes;
-    ////the index of the next note to be spawned
-    //[SerializeField] int nextIndex = 0;
-    //[SerializeField]
-    ////the current position of the song (in seconds)
-    //float songPosition,
-    ////the current position of the song (in beats)
-    //songPosInBeats,
-    ////the duration of a beat
-    //secPerBeat,
-    ////how much time (in seconds) has passed since the song started
-    //dsptimesong;
-
-    //bool isReady = false;
-    //bool hadStarted = false;
-    //private float elapsedTime = 0f;
-    //float ghostPositionIndex = 0;
-    //Vector3[] ghostTargetPosition = new Vector3[3];
-
-    //// Start is called before the first frame update
-    //void Start()
-    //{
-
-    //}
-
-    //// Update is called once per frame
-    //void Update()
-    //{
-    //    if (!isReady)
-    //    {
-    //        elapsedTime += Time.deltaTime;
-    //        if (elapsedTime >= 2f)
-    //        {
-    //            isReady = true;
-    //            dsptimesong = (float)AudioSettings.dspTime;
-    //            SpawnGhost();
-    //        }
-    //    }
-
-    //    if (isReady)
-    //    {
-    //        if (!hadStarted)
-    //        {
-    //            //calculate how many seconds is one beat
-    //            //we will see the declaration of bpm later
-    //            secPerBeat = 60f / bpm;
-
-    //            //Add hihat in song
-    //            StartSong();
-    //            //record the time when the song starts
-    //            hadStarted = true;
-    //        }
-    //        //calculate the position in seconds
-    //        songPosition = (float)(AudioSettings.dspTime);
-
-    //        //calculate the position in beats
-    //        songPosInBeats = songPosition / secPerBeat;
-    //        MovePlayer();
-    //        if (songPosInBeats % 1 == 0)
-    //        {
-    //            MoveGhost();
-    //            ghostPositionIndex++;
-    //        }
-    //    }
-    //}
-
-    //void MovePlayer()
-    //{
-    //    if (Mathf.Round(songPosInBeats) - songPosInBeats < 0.140f && Mathf.Round(songPosInBeats) - songPosInBeats > -0.060f)
-    //    {
-    //        Debug.Log(songPosInBeats);
-    //        if (Input.GetKeyDown(KeyCode.W) && player.transform.position.y < 8)
-    //        { player.transform.Translate(0f, 1f, 0); }
-    //        if (Input.GetKeyDown(KeyCode.A) && player.transform.position.x > 0)
-    //        { player.transform.Translate(-1f, 0f, 0); }
-    //        if (Input.GetKeyDown(KeyCode.S) && player.transform.position.y > 0)
-    //        { player.transform.Translate(0f, -1f, 0); }
-    //        if (Input.GetKeyDown(KeyCode.D) && player.transform.position.x < 15)
-    //        { player.transform.Translate(1f, 0f, 0); }
-    //    }
-
-    //}
-
-    //public void MoveGhost()
-    //{
-    //    if (ghostPositionIndex > 2)
-    //    {
-    //        switch (ghostPositionIndex % 3)
-    //        {
-    //            case 0:
-    //                ghostTargetPosition[0] = player.transform.position;
-    //                break;
-    //            case 1:
-    //                ghostTargetPosition[1] = player.transform.position;
-    //                break;
-    //            case 2:
-    //                ghostTargetPosition[2] = player.transform.position;
-    //                break;
-    //        }
-    //    }
-    //}
-
-    //void StartSong()
-    //{
-    //    song.Play();
-    //}
-
-    //void SpawnGhost()
-    //{
-    //    ghost.transform.position = new Vector3(0f, 8f, 0f);
-    //}
-
-
-
-
-    //void CountdownEvent()
-    //{
-    //    switch (songPosInBeats)
-    //    {
-    //        case 1:
-    //            countdown.text = "3";
-    //            break;
-    //        case 2:
-    //            countdown.text = "2";
-    //            break;
-    //        case 3:
-    //            countdown.text = "1";
-    //            break;
-    //        case 4:
-    //            countdown.text = "GO!";
-    //            break;
-    //    }
-    //}
+    public void CheckForNewInterval(float interval)
+    {
+        if (Mathf.FloorToInt(interval) != lastInterval)
+        {
+            lastInterval = Mathf.FloorToInt(interval);
+            trigger.Invoke();
+        }
+    }
 }
